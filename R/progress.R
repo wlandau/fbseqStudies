58a118008c1c206d4bba1643c7d39c2abacca070
wlandau <will.landau@gmail.com>
2015-11-19 15:31:51 -0600

    progress

---
+++ R/progress.R
@@ -0,0 +1,17 @@
+#' @title Function \code{progress}
+#' @description check on progress of simulation study
+#' @export
+#' @return path to simulated objects
+progress = function(path){
+  path = newdir(path)
+  n = NULL
+  i = 0
+  for(f in list.files(path)){
+    i = i + 1
+    s = readRDS(paste0(path, f))
+    print(c(i, f, names(s$analyses)))
+    n = c(n, names(s$analyses))
+  }
+  print(table(n))
+  path
+}
