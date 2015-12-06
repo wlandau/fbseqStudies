887106d70c9aca02c2a0e9115fbc1ef2139a7515
wlandau <will.landau@gmail.com>
2015-12-05 20:04:48 -0600

    .

--- R/case.R
+++ R/case.R
@@ -4,8 +4,6 @@ NULL
 #' @title Function \code{case}
 #' @description Whole orkflow for the part of the case study paper that looks at gene detection power
 #' @export
-#' @return path to simulated objects
-#' @param path to directory to save simulations and results
 case = function(){
   real("real")
   case_coverage("case_coverage")
