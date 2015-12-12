c536616d1827aefa36d370b0192b9693bdb54c30
wlandau <will.landau@gmail.com>
2015-12-12 10:31:36 -0600

    .

--- R/repair.R
+++ R/repair.R
@@ -1,13 +1,13 @@
-#' @title Function \code{rocs}
+#' @title Function \code{repair}
 #' @description make repairs to names, etc. in a single simulation list
 #' @export
 #' @param l simulation list
 repair = function(l){
   for(a in names(l$analyses)){
     if(!any(grepl("prob_", colnames(l$analyses[[a]]$estimates))))
-      colnames(l$analyses[[a]]$estimates)[6:10] = paste0("prob_", colnames(l$analyses[[a]]$estimates)[6:10])
+      colnames(l$analyses[[a]]$estimates)[6:11] = paste0("prob_", colnames(l$analyses[[a]]$estimates)[6:11])
     if(!any(grepl("effect_", colnames(l$analyses[[a]]$estimates))))
-      colnames(l$analyses[[a]]$estimates)[11:15] = paste0("effect_", colnames(l$analyses[[a]]$estimates)[11:15])
+      colnames(l$analyses[[a]]$estimates)[12:17] = paste0("effect_", colnames(l$analyses[[a]]$estimates)[12:17])
     if("chains" %in% names(l$analyses[[a]])){
       l$analyses[[a]]$psrf = psrf(l$analyses[[a]]$chains)
       l$analyses[[a]]$ess = effectiveSize(mcmc_samples(l$analyses[[a]]$chains))
@@ -16,7 +16,7 @@ repair = function(l){
   l
 }
 
-#' @title Function \code{rocs}
+#' @title Function \code{repair}
 #' @description make repairs to names, etc. to simulation lists
 #' @export
 #' @param from directory with simulation lists
