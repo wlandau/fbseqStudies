7ac9a4ae650958aed987552c11b37886a2951e9f
wlandau <will.landau@gmail.com>
2015-12-20 01:34:19 -0600

    .

--- R/analyze-mytheme.R
+++ R/analyze-mytheme.R
@@ -3,7 +3,7 @@
 #' @export
 #' @return ggplot2 theme
 mytheme = function(){
-  theme(axis.text = element_text(family = "Helvetica", colour = 'black', angle = -80, hjust = 0),
+  theme(axis.text.x = element_text(family = "Helvetica", colour = 'black', angle = -80, hjust = 0),
                   panel.background = element_rect(fill='white'),
                   panel.border = element_rect(color="black", fill = NA),
                   panel.grid.major = element_line(color="lightgray"),
