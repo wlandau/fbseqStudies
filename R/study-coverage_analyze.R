884eb0560bb508d7046f3e164ddc6c7bff74a019
wlandau <will.landau@gmail.com>
2015-12-20 11:49:51 -0600

    plotting

--- R/study-coverage_analyze.R
+++ R/study-coverage_analyze.R
@@ -15,6 +15,7 @@ coverage_analyze = function(from, to){
   ci_beta(paste0(to, "ci_beta_list"), paste0(to, "ci_beta"))
   rocs(from, paste0(to, "roc"))
   plot_roc(paste0(to, "roc"), paste0(to, "plot_roc"))
+  plot_auc(paste0(to, "roc"), paste0(to, "auc"))
   calibrations(from, paste0(to, "calibration"))
   plot_calibration(paste0(to, "calibration"), paste0(to, "plot_calibration"))
 }
