2108dbb12cfaa931af304ea5a731e85c82dd801a
wlandau <will.landau@gmail.com>
2015-12-20 18:04:10 -0600

    rename

---
+++ R/util-myrelevel.R
@@ -0,0 +1,23 @@
+#' @title Function \code{myrelevel}
+#' @description relevel analysis vector for plotting labels
+#' @export
+#' @param x factor to relevel
+#' @return releveled factor
+myrelevel = function(x){
+  out = as.character(x)
+  out[out == "ebayesFromTruth+normal"] = "eBayes (oracle)"
+  out[out == "ebayesFromStarts+normal"] = "eBayes (naive)"
+  out[out == "ebayesFromFullybayes+normal"] = "eBayes (posterior)"
+  out[out == "fullybayes+normal"] = "fully Bayes (normal)"
+  out[out == "fullybayes+Laplace"] = "fully Bayes (Laplace)"
+  out[out == "fullybayes+t"] = "fully Bayes (t)"
+  out[out == "fullybayes+horseshoe"] = "fully Bayes (horseshoe)"
+  ordered(out, levels = c(
+    "eBayes (oracle)", 
+    "eBayes (naive)",
+    "eBayes (posterior)",
+    "fully Bayes (normal)",
+    "fully Bayes (Laplace)",
+    "fully Bayes (t)",
+    "fully Bayes (horseshoe)"))
+}
