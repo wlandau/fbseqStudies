de8dbb9cfa2a8095506a209eebc012c6072084de
wlandau <will.landau@gmail.com>
2016-05-04 22:11:58 -0500

    Add scaledown function

---
+++ R/util-scaledown.R
@@ -0,0 +1,22 @@
+#' @title Function \code{scaledown}
+#' @description Scale down to a short workflow by 
+#' reducing the number of genes in data
+#' and iterations of MCMCs
+#' @export
+scaledown = function(){
+  if(!getOption("fbseqStudies.scaledown")) return()
+  options("fbseqStudies.backend" = "serial")
+  genes = 30
+  assign("genes", 30, envir = parent.frame())
+  assign("iterations", 100, envir = parent.frame())
+  assign("burnin", 100, envir = parent.frame())
+  assign("thin", 100, envir = parent.frame())
+
+  d = get("paschold")
+  d@counts = head(d@counts, genes)
+  assign("paschold", d, envir = parent.frame())
+
+  d = get("tableS3table1")
+  d = head(d, genes)
+  assign("tableS3table1", d, envir = parent.frame())
+}
--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,6 @@
-#' @title .onLoad
-#' @description fbseqStudies .onLoad
-#' @export
-#' @param libname libname
-#' @param pkgname pkgname
 .onLoad = function(libname, pkgname){
   options(list(
-    "fbseqStudies.scaledown" = F,
-    "fbseqStudies.scaledown.genes" = 30,
-    "fbseqStudies.scaledown.iterations" = 100,
-    "fbseqStudies.scaledown.burnin" = 100,
-    "fbseqStudies.scaledown.thin" = 1
+    "fbseqStudies.backend" = "CUDA",
+    "fbseqStudies.scaledown" = F
   ))
 }
