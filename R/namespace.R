6faa510c22f67546417e3a54e0443405274b2a7a
wlandau <will.landau@gmail.com>
2016-06-20 16:46:39 -0400

    Add packages to imports

--- R/namespace.R
+++ R/namespace.R
@@ -1,11 +1,14 @@
 #' @importFrom actuar dinvgamma
 #' @importFrom plyr adply ddply mapvalues
 #' @importFrom dplyr %>% group_by_ mutate rename summarize_
+#' @importFrom grDevices cairo_ps dev.off pdf postscript setEPS
 #' @importFrom gridExtra grid.arrange
 #' @importFrom pracma trapz
 #' @importFrom readr read_csv
 #' @importFrom reshape2 melt
 #' @importFrom rstan sampling stan_model
+#' @importFrom stats as.formula dnorm ksmooth median qnorm quantile rexp rgamma rnbinom rnorm rpois sd stepfun
+#' @importFrom utils data head write.csv
 #' @import coda
 #' @import doMC
 #' @import doParallel
